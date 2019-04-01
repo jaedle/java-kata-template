34019cd9ac8a91234040db71582425fe5eddb71c
jaedle <dennis.jekubczyk@gmail.com>
2019-04-01 06:10:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Template for Java TDD Kata
+
+## Cloning
+
+```sh
+git clone https://github.com/jaedle/java-kata-template
+```
+
+## Preparing for Kata
+
+```sh
+rm -r .git/
+```
+
+## Get started
+
+:)
---
+++ src/test/java/com/example/kata/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.example.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+    @Test
+    public void returnsTrue() {
+        assertTrue("someLibraryMethod should return 'true'", new Example().someLibraryMethod());
+    }
+}
